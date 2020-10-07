13395819b5685b69b494f7d6813afa4bb1732219
Мария Матичина <>
2020-10-07 22:22:03 +0300

    Создала проект

---
+++ OnlyOfficeTest/OnlyOfficeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnlyOfficeTest
+//
+//  Created by Мария Матичина on 10/7/20.
+//  Copyright © 2020 Мария Матичина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
